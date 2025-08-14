e0256261c2c90126dabd248b1962a010e5854ed2
max23esau <esa-max23@live.com>
2025-08-14 14:14:57 -0600

    Swiper js added to Product grid

--- src/components/ProductGrid/ProductGrid.jsx
+++ src/components/ProductGrid/ProductGrid.jsx
@@ -1,7 +1,13 @@
 import { useState, useEffect } from "react";
-import ProductCard from '../ProductCard/ProductCard';
-import './ProductGrid.css'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Keyboard } from 'swiper/modules';
 
+import ProductCard from '../ProductCard/ProductCard';
+
+import './ProductGrid.css'
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 
 
@@ -21,18 +27,58 @@ const ProductGrid = ({ title, textLink, url }) => {
     <section className="productGrid">
       <div className="productGrid__container">
         <div className="productGrid__container-heading">
-          <h2 className="">{title}</h2>
-          <a href={url} target="_blank" rel="noopener noreferrer">{textLink}</a>
-        </div>
-        <div className="productGrid__container-total-items">
-          <p className="">{products.length} items</p>
+          <div className="productGrid__container-deails">
+            <h2 className="">{title}</h2>
+            <a href={url} target="_blank" rel="noopener noreferrer">{textLink}</a>
+          </div>
+          <div className="productGrid__container-total-items">
+            <p className="">{products.length} items</p>
+            <div className="swiper-nav-buttons">
+              <button className="swiper-button-prev-custom"></button>
+              <button className="swiper-button-next-custom"></button>
+            </div>
+          </div>
         </div>
         <div className="productGrid__cards-container">
-          <div className="productGrid__cards-items">
-            {products.map((product, index) => (
-              <ProductCard key={index} product={product} />
-            ))}
-          </div>
+          {/* <div className="productGrid__cards-items"> */}
+            <Swiper
+              slidesPerView={4}
+              spaceBetween={16}
+              grabCursor={true}
+              navigation={{
+                prevEl: ".swiper-button-prev-custom",
+                nextEl: ".swiper-button-next-custom",
+              }}
+              breakpoints={{
+                200: {
+                  slidesPerView: 1.5,
+                  spaceBetween: 16,
+                },
+                500: {
+                  slidesPerView: 1.5,
+                  spaceBetween: 16,
+                },
+                768: {
+                  slidesPerView: 3,
+                  spaceBetween: 16,
+                },
+                991: {
+                  slidesPerView: 4,
+                  spaceBetween: 16,
+                },
+              }}
+              mousewheel={true}
+              keyboard={true}
+              modules={[Navigation, Pagination, Keyboard]}
+              className="mySwiper"
+            >
+              {products.map((product, index) => (
+              <SwiperSlide>
+                <ProductCard key={index} product={product} />
+              </SwiperSlide>
+              ))}
+            </Swiper>
+          {/* </div> */}
         </div>
       </div>
     </section>
